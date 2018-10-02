73275117aff2b51edc5444a12626c1b9d281a935
Julian Wolf <julian.w16@gmail.com>
2018-10-02 13:26:18 +0200

    auto commit

--- D01/ex11/do_op_2.php
+++ D01/ex11/do_op_2.php
@@ -46,26 +46,26 @@
 		$ret = array_filter(preg_split('/\s+/', $str));
 		return ($ret);
 	}
-
 	if ($argc == 2)
 	{
-		$a1 = escapeshellarg($argv[1]);
-		$a2 = escapeshellcmd($a1);
-		$a3 = trim($a2, " ");
-		$split = ft_split($a3);
+		$split = ft_split($argv[1]);
 		if (is_numeric($split[0]) && is_numeric($split[2]))
 		{
 			if ($split[1] == "+")
 				echo add(trim($split[0], " \t"), trim($split[2], " \t")) . "\n";
-			if ($split[1] == "-")
+			else if ($split[1] == "-")
 				echo minus(trim($split[0], " \t"), trim($split[2], " \t")) . "\n";
-			if ($split[1] == "*")
+			else if ($split[1] == "*")
 				echo mult(trim($argv[0], " \t"), trim($split[2], " \t")) . "\n";
-			if ($split[1] == "/")
+			else if ($split[1] == "/")
 				echo divide(trim($split[0], " \t"), trim($split[2], " \t")) . "\n";
-			if ($split[1] == "%")
+			else if ($split[1] == "%")
 				echo mod(trim($split[0], " \t"), trim($split[2], " \t")) . "\n";
+			else
+				echo "Syntax Error\n";
 		}
+		else
+			echo "Syntax Error\n";
 	}else
 		echo "Incorrect Parameters\n";
 ?>
