b4e359579b7ef6ece0ea8041af5ec18df3df9e6f
Julian Wolf <julian.w16@gmail.com>
2018-10-03 18:02:35 +0200

    auto commit

--- D02/ex03/who.php
+++ D02/ex03/who.php
@@ -4,22 +4,22 @@
 	$file = fopen("/var/run/utmpx", "r");
 	while	($in = fread($file, 628))
 	{
-		$content = unpack("a256user/a4id/a32line/ipid/itype/I2time", $in);
-		if	($content["type"] == 7)
-			$ret[] = $content;
+		$info = unpack("a256user/a4id/a32line/ipid/itype/I2time", $in);
+		if	($info["type"] == 7)
+			$ret[] = $info;
 	}
 	fclose($file);
 	$len = 0;
-	foreach ($ret as $elem)
-		if	(count(substr($elem["user"], 0, strpos($elem["user"], 0))) > $len)
-			$len = strlen(substr($elem["user"], 0, strpos($elem["user"], 0)));
+	foreach ($ret as $result)
+		if	(count(substr($result["user"], 0, strpos($result["user"], 0))) > $len)
+			$len = strlen(substr($result["user"], 0, strpos($result["user"], 0)));
 
 	$len = max($len + 3 - ($len % 3), 9);
 
-	foreach ($ret as $elem)
+	foreach ($ret as $result)
 	{
-		echo str_pad(substr($elem["user"], 0, strpos($elem["user"], 0)), $len);
-		echo substr($elem["line"], 0, strpos($elem["line"], 0))."  ";
-		echo date("M  j h:i", $elem["time1"])." \n";
+		echo str_pad(substr($result["user"], 0, strpos($result["user"], 0)), $len);
+		echo substr($result["line"], 0, strpos($result["line"], 0))."  ";
+		echo date("M  j h:i", $result["time1"])." \n";
 	}
 ?>
