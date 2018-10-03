49f1ab5d2779c9cd3ff0772896b93e25e1e16a58
Julian Wolf <julian.w16@gmail.com>
2018-10-03 07:35:41 +0200

    auto commit

--- D01/ex11/do_op_2.php
+++ D01/ex11/do_op_2.php
@@ -43,12 +43,37 @@
 
 	function ft_split($str)
 	{
+		$i = 0;
+		$sign = NULL;
+		while ($str[$i])
+		{
+			if (ord($str[$i]) == ord("+") || ord($str[$i]) == ord("-") || ord($str[$i]) == ord("*") ||
+				ord($str[$i]) == ord("/") || ord($str[$i]) == ord("%"))
+			{
+				echo "Found Sign";
+				$sign = $str[$i];
+				break ;
+			}
+			$i++;
+		}
+		var_dump($sign);
 		$ret = array_filter(preg_split('/\s+/', $str));
+		var_dump($ret);
+		if (count($ret) != 3)
+		{
+			$ret2 = explode($sign, $str);
+			var_dump($ret2);
+			$hold = $ret2[1];
+			$ret2[1] = $sign;
+			$ret2[2] = $hold;
+			return ($ret2);
+		}
+		var_dump($ret);
 		return ($ret);
 	}
 	if ($argc == 2)
 	{
-		$split = ft_split($argv[1]);
+		$split = ft_split(trim($argv[1], " \t"));
 		if (is_numeric($split[0]) && is_numeric($split[2]))
 		{
 			if ($split[1] == "+")
