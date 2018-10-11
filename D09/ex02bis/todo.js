08d44f26a8c83fdcb2725bfef9da035fa2a71094
Julian Wolf <julian.w16@gmail.com>
2018-10-11 17:26:58 +0200

    auto commit

---
+++ D09/ex02bis/todo.js
@@ -0,0 +1,44 @@
+var ft_list;
+var cookie = new Array();
+
+$(document).ready(function(){
+	$('#New').click(newI);
+	var temp = document.cookie;
+	$('.ft_list div').click(del);
+    ft_list = $('.ft_list');
+	if (temp){
+		cookie = JSON.parse(temp);
+		cookie.forEach(function (e)
+		{
+			add(e);
+		});
+	}
+})
+
+function save() {
+	var items = ft_list.children();
+	var newCook = [];
+	for (var i = 0; i < items.length; i++)
+		newCook.push(items[i].innerHTML);
+	document.cookie = JSON.stringify(newCook);
+}
+
+
+function newI()
+{
+	var info = prompt("What would you like to do?", "");
+	if (info)
+		add(info);
+	save();
+}
+
+function add(info)
+{
+	ft_list.prepend($('<div>' + info + '</div>').click(del));
+}
+
+function del()
+{
+	if (confirm("Purge the matrix??"))
+		this.remove();
+}
